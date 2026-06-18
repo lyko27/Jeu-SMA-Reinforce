a3a1db8d19442341e6c0c630e53c0f32b1860120
Nateo <bouff3000@gmail.com>
2026-06-18 15:40:54 +0200

    fichier.h du monde pour la structure du monde

---
+++ monde.h
@@ -0,0 +1,32 @@
+#ifndef MONDE_H
+#define MONDE_H
+
+typedef enum {
+    FERMIER,
+    CHEVRE,
+} TypeEntite;
+
+typedef struct {
+    float x;
+    float y;
+} Vecteur2D;
+
+// une entitée
+typedef struct {
+    Vecteur2D position;
+    Vecteur2D velocite; // La direction et la vitesse actuelles
+    TypeEntite type;
+    int en_vie;         // 1 si actif, 0 si mort (à retirer du tableau)
+} entitee_t;
+
+// Le Monde 
+typedef struct {
+    int largeur;
+    int hauteur;
+    entitee_t * entites;      // Tableau dynamique (malloc) contenant TOUS les agents
+    int nb_entites;      // Nombre actuel d'entitée
+    int capacite_max;   // Taille allouée pour le tableau (pour ajouter des bébés chèvres)
+                            // L'économie du joueur...
+} monde;
+
+#endif 

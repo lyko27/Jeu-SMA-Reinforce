bd5bd4d2db14f798d746b7fb4ca9c253192ee78a
Nateo <bouff3000@gmail.com>
2026-06-18 23:25:30 +0200

    supression des leak de mémoire

--- maitre_du_jeu.c
+++ maitre_du_jeu.c
@@ -18,13 +18,13 @@
 /* ENtrées : le tableau de chèvre
     Sotie : aucune
     Synopsis : libère toute les goats du tableau*/
-void free_goat(Goat ** goat)
+void free_goat(Goat **goat)
 {
-    if(goat != NULL)
+    if (goat != NULL)
     {
         int i = 0;
-        Goat * current_goat = goat[i];
-        while(current_goat != NULL)
+        Goat *current_goat = goat[i];
+        while (current_goat != NULL)
         {
             free(current_goat);
             i++;
@@ -36,9 +36,9 @@ void free_goat(Goat ** goat)
 /* ENtrées : le monde actuel dans sa structure et une chèvre
     Sotie : Le monde mis a jour avec la nouvelle chèvre en plus
     Synopsis : prend une chèvre et l'ajoute au monde*/
-monde * ajouter_goat(monde * monde_courant, Goat * goat)
+monde *ajouter_goat(monde *monde_courant, Goat *goat)
 {
-    if(monde_courant->nb_goat+1 > monde_courant->capacite_max_goat)
+    if (monde_courant->nb_goat + 1 > monde_courant->capacite_max_goat)
     {
         monde_courant->goats_tab = realloc(monde_courant->goats_tab, monde_courant->capacite_max_goat * 2 * sizeof(Goat));
         monde_courant->capacite_max_goat *= 2;
@@ -51,17 +51,18 @@ monde * ajouter_goat(monde * monde_courant, Goat * goat)
 /* Entrée : deux entier la largeur et la hauteur du monde
    Sortie : le monde vide
    synopsis : créer un monde vide avec la structure monde*/
-monde * creer_monde(int largeur, int hauteur)
+monde *creer_monde(int largeur, int hauteur)
 {
-    monde * monde_courant = malloc(sizeof(monde));
-    if(monde_courant)
+    monde *monde_courant = malloc(sizeof(monde));
+    if (monde_courant)
     {
         monde_courant->largeur = largeur;
         monde_courant->hauteur = hauteur;
         monde_courant->capacite_max_goat = 100;
         monde_courant->nb_goat = 0;
-        monde_courant->goats_tab = malloc(monde_courant->capacite_max_goat * sizeof(Goat*));
-        if(monde_courant->goats_tab) return monde_courant;
+        monde_courant->goats_tab = malloc(monde_courant->capacite_max_goat * sizeof(Goat *));
+        if (monde_courant->goats_tab)
+            return monde_courant;
         else
         {
             free(monde_courant->goats_tab);
@@ -69,7 +70,7 @@ monde * creer_monde(int largeur, int hauteur)
             return NULL;
         }
     }
-    else 
+    else
     {
         free(monde_courant);
         return NULL;
@@ -79,10 +80,10 @@ monde * creer_monde(int largeur, int hauteur)
 /* Entrée : le monde actuel, normalement complètement vide
     Sortie : le monde avec le fermier et 10 chèvre qui apparaisse aléatoirement
     Synopsis : remplie le monde de 10 chèvre à des endroits aléatoire et le fermier devant sa maison */
-monde * generer_un_monde(monde * monde_courant)
+monde *generer_un_monde(monde *monde_courant)
 {
-    Fermier * le_fermier = malloc(sizeof(Fermier));
-    if(le_fermier)
+    Fermier *le_fermier = malloc(sizeof(Fermier));
+    if (le_fermier)
     {
         le_fermier->frame = 0;
         le_fermier->direction_sprite = 2;
@@ -94,10 +95,10 @@ monde * generer_un_monde(monde * monde_courant)
         free(le_fermier);
         return NULL;
     }
-    for (int i = 0 ; i<1 ; i++)
+    for (int i = 0; i < 1; i++)
     {
-        Goat * une_goat = malloc(sizeof(Goat));
-        if(une_goat)
+        Goat *une_goat = malloc(sizeof(Goat));
+        if (une_goat)
         {
             une_goat->frame = 0;
             une_goat->direction_sprite = 2;
@@ -116,22 +117,26 @@ monde * generer_un_monde(monde * monde_courant)
 /* Entrée : le monde actuel
     Sortie : aucune
     Synopsis : prend le monde et pour chaque entité, demande à la SDL d'annimer 4 frame de l'entité pour qu'elle se déplacent sur l'écran*/
-void afficher_monde(monde * monde_courant)
+void afficher_monde(monde *monde_courant)
 {
-    int pas_goat = monde_courant->goats_tab[0]->speed/4;
-    for(int j = 0; j<4; j++)
+    int pas_goat = monde_courant->goats_tab[0]->speed / 4;
+    for (int j = 0; j < 4; j++)
     {
         dessiner_monde();
-        for(int w = 0; w<monde_courant->nb_goat; w++)
+        for (int w = 0; w < monde_courant->nb_goat; w++)
         {
-            Goat * current_goat = monde_courant->goats_tab[w];
-            dessiner_entite(1, current_goat->dir_x - pas_goat *4-j, current_goat->dir_y - pas_goat *4-j,current_goat->frame, current_goat->direction_sprite);
-            if(current_goat->frame == 4) current_goat->frame = 0;
-            else current_goat->frame++;
+            Goat *current_goat = monde_courant->goats_tab[w];
+            dessiner_entite(1, current_goat->dir_x - pas_goat * 4 - j, current_goat->dir_y - pas_goat * 4 - j, current_goat->frame, current_goat->direction_sprite);
+            if (current_goat->frame == 4)
+                current_goat->frame = 0;
+            else
+                current_goat->frame++;
         }
-        dessiner_entite(2, monde_courant->fermiers->x - pas_goat *4-j, monde_courant->fermiers->y-pas_goat *4-j, monde_courant->fermiers->frame, monde_courant->fermiers->direction_sprite);
-        if(monde_courant->fermiers->frame == 9) monde_courant->fermiers->frame = 0;
-        else monde_courant->fermiers->frame++;
+        dessiner_entite(2, monde_courant->fermiers->x - pas_goat * 4 - j, monde_courant->fermiers->y - pas_goat * 4 - j, monde_courant->fermiers->frame, monde_courant->fermiers->direction_sprite);
+        if (monde_courant->fermiers->frame == 9)
+            monde_courant->fermiers->frame = 0;
+        else
+            monde_courant->fermiers->frame++;
         actualiser_ecran();
         SDL_Delay(200);
     }
@@ -140,11 +145,11 @@ void afficher_monde(monde * monde_courant)
 /* Entrée : le monde actuel
     Sortie : le monde mis à jour
     Synopsis : Met à jour la position de chaque entité après déplacement dans notre jeu */
-monde * mis_à_jour_monde(monde * monde_courant)
+monde *mis_à_jour_monde(monde *monde_courant)
 {
-    for(int i = 0 ; i < monde_courant->nb_goat ; i++)
+    for (int i = 0; i < monde_courant->nb_goat; i++)
     {
-        Goat * current_goat = monde_courant->goats_tab[i];
+        Goat *current_goat = monde_courant->goats_tab[i];
         current_goat = update_goat(current_goat, monde_courant->goats_tab, monde_courant->nb_goat);
         current_goat->x = current_goat->dir_x;
         current_goat->y = current_goat->dir_y;
@@ -159,43 +164,43 @@ int main(int argc, char **argv)
     (void)argv;
     srand(time(NULL));
     // création et initialisation du monde
-    monde * monde_courrant = creer_monde(LARGEUR, HAUTEUR);
+    monde *monde_courrant = creer_monde(LARGEUR, HAUTEUR);
     monde_courrant = generer_un_monde(monde_courrant);
     int quiiter_le_programme = 0; // variable de gestion de la boucle
     int en_pause = 0;
     init_affichage();
     while (quiiter_le_programme == 0)
     {
-        if (!en_pause)
+        /* recuperation des  input utilisateur */
+        interaction_utilisateur *utilisateur = malloc(sizeof(interaction_utilisateur));
+        if (utilisateur)
         {
-            /* recuperation des  input utilisateur */
-            interaction_utilisateur * utilisateur = malloc(sizeof(interaction_utilisateur));
-            if (utilisateur)
-            {
-                utilisateur->x_deplacement = 0;
-                utilisateur->y_deplacement = 0;
-                utilisateur->pause = 0;
-                utilisateur->quitter = 0;
-                utilisateur = recuperer_mouvement(utilisateur);
-                quiiter_le_programme = utilisateur->quitter; // si quitter = 1 le programme prendra fin à a la fin de la boucle
+            utilisateur->x_deplacement = 0;
+            utilisateur->y_deplacement = 0;
+            utilisateur->pause = 0;
+            utilisateur->quitter = 0;
+            utilisateur = recuperer_mouvement(utilisateur);
+            quiiter_le_programme = utilisateur->quitter; // si quitter = 1 le programme prendra fin à a la fin de la boucle
 
-                if (utilisateur->pause)
-                {
-                    en_pause = !en_pause;
-                }
+            if (utilisateur->pause)
+            {
+                en_pause = !en_pause;
+            }
+            if (!en_pause)
+            {
                 // déplacement du fermier au cas où l'utilsateur avait appuyé sur une des flèche
                 monde_courrant->fermiers = update_fermier(monde_courrant->fermiers, utilisateur->x_deplacement, utilisateur->y_deplacement);
-
-                afficher_monde(monde_courrant);
-
-                free(utilisateur);
-                SDL_Delay(200);
             }
+            afficher_monde(monde_courrant);
+            mis_à_jour_monde(monde_courrant);
+            free(utilisateur);
+            SDL_Delay(200);
         }
     }
     quitter_affichage();
     free_goat(monde_courrant->goats_tab);
     free(monde_courrant->goats_tab);
+    free(monde_courrant->fermiers);
     free(monde_courrant);
     return 0;
 }

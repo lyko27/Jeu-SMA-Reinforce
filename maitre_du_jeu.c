95acda4e2259851bf68e9c4596074d083defd6b0
Nateo <bouff3000@gmail.com>
2026-06-18 15:39:31 +0200

    ajout de la fonction creer monde dans mâitre du jeu

--- maitre_du_jeu.c
+++ maitre_du_jeu.c
@@ -4,92 +4,70 @@
 #include "utilisateur.h"
 #include "affichage.h"
 #include "fermier.h"
+#include "monde.h"
 
 #define LARGEUR 200
 #define HAUTEUR 200
 
+
+/* Entrée : deux entier la largeur et la hauteur du monde
+   Sortie : le monde vide
+   synopsis : créer un monde vide avec la structure monde*/
+monde * creer_monde(int largeur, int hauteur)
+{
+    monde * monde_courant = malloc(sizeof(monde));
+    if(monde_courant)
+    {
+        monde_courant->largeur = largeur;
+        monde_courant->hauteur = hauteur;
+        monde_courant->capacite_max = 100;
+        monde_courant->nb_entites = 0;
+        monde_courant->entites = malloc(monde_courant->capacite_max * sizeof(entitee_t));
+        if(monde_courant->entites) return monde_courant;
+        else
+        {
+            free(monde_courant->entites);
+            free(monde_courant);
+            return NULL
+        }
+    }
+    else 
+    {
+        free(monde_courant);
+        return NULL;
+    }
+}
 
 int main(int argc, char **argv)
 {
     (void)argc;
     (void)argv;
 
-    /* Création des tableaux_click de cases de taille TAILLE par TAILLE, soit 10px par case et 20 case par ligne */
-    int *monde_courrant = calloc(HAUTEUR * LARGEUR, sizeof(int));
-
     int quiiter_le_programme = 0; // variable de gestion de la boucle
     int en_pause = 1;
     while (quiiter_le_programme == 0)
     {
-        int *monde_suivant = calloc(HAUTEUR * LARGEUR, sizeof(int));
+        
 
         if (!en_pause)
         {
-        evenement_clique_pause *resultat = malloc(sizeof(evenement_clique_pause));
-        if (resultat)
-        {
-            resultat->zoom = 0;
-            resultat->quitter = 0;
-            resultat->pause = 0;
-            resultat->deplacement_x = 0;
-            resultat->deplacement_y = 0;
-            resultat->x_zoom = -1;
-            resultat->y_zoom = -1;
-            resultat = recuperer_entree_clique(resultat);
-            quiiter_le_programme = resultat->quitter; // si quitter = 1 le programme prendra fin à a la fin de la boucle
-
-
-            if(resultat->zoom !=0)
+            interaction_utilisateur * utilisateur = malloc(sizeof(interaction_utilisateur));
+            if (utilisateur)
             {
-                float old_zoom = zoom;
-                zoom += resultat->zoom * 0.1f; // on zoom sur la fenêtre
-                if (zoom <= 0.2f)
-                    zoom = 0.2f; // on empêche le zoom de devenir nul ou négatif
+                utilisateur->deplacement_x = 0;
+                utilisateur->deplacement_y = 0;
+                utilisateur = recuperer_mouvement(utilisateur);
+                quiiter_le_programme = utilisateur->quitter; // si quitter = 1 le programme prendra fin à a la fin de la boucle
 
-                if (zoom != old_zoom && resultat->x_zoom >= 0 && resultat->y_zoom >= 0)
+                if (utilisateur->pause)
                 {
-                    float ratio = zoom / old_zoom;
-                    offset_x = (resultat->x_zoom - 400.0f) - (resultat->x_zoom - 400.0f - offset_x) * ratio;
-                    offset_y = (resultat->y_zoom - 400.0f) - (resultat->y_zoom - 400.0f - offset_y) * ratio;
+                    en_pause = !en_pause;
                 }
+                dessiner_monde(monde_courrant, offset_x, offset_y, zoom);
+                free(utilisateur);
+                SDL_Delay(100);
             }
-
-            offset_x += resultat->deplacement_x * VITESSE_DE_MOUVEMENt;
-            offset_y += resultat->deplacement_y * VITESSE_DE_MOUVEMENt;
-
-            if (resultat->pause)
-            {
-                en_pause = !en_pause;
-            }
-
-            if (resultat->x_click != -1 && resultat->y_click != -1)
-            {
-                float TAILLE_CELLULE_ZOOM = 10.0f * zoom;
-                float position_x = (800 - (TAILLE * TAILLE_CELLULE_ZOOM)) / 2.0f + offset_x;
-                float position_y = (800 - (TAILLE * TAILLE_CELLULE_ZOOM)) / 2.0f + offset_y;
-
-                if (resultat->x_click >= position_x && resultat->y_click >= position_y)
-                {
-                    int grid_x = (resultat->x_click - position_x) / TAILLE_CELLULE_ZOOM;
-                    int grid_y = (resultat->y_click - position_y) / TAILLE_CELLULE_ZOOM;
-
-                    if (grid_x >= 0 && grid_x < TAILLE && grid_y >= 0 && grid_y < TAILLE)
-                    {
-                        // On inverse l'état de la case au clic (vivante <-> morte)
-                        monde_suivant[grid_x + TAILLE * grid_y] = !monde_suivant[grid_x + TAILLE * grid_y];
-                    }
-                }
-            }
-            /*intervertion des monde*/
-            int *tmp = monde_courrant;
-            monde_courrant = monde_suivant;
-            free(tmp);
-
-            dessiner_monde(monde_courrant, offset_x, offset_y, zoom);
-            free(resultat);
-            SDL_Delay(100);
         }
-    }
-    end_sdl(1, "Fin normale du programme");
     return 0;
+    }
 }

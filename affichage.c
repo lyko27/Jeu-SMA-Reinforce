fd2276744b65b3b54d390189100bbe36bfdc61c3
sohail <sohail.schl@gmail.com>
2026-06-18 16:54:25 +0200

    changement affichage ne gere pas le deplacement des entites il recoit juste quelle frame dessiner et ou la dessiner

--- affichage.c
+++ affichage.c
@@ -12,28 +12,21 @@ SDL_Texture *texture_fermier = NULL;
 
 
 
-int init_affichage(){
-    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-            printf("Erreur SDL_Init : %s\n", SDL_GetError());
-            return 1;
-        }
-
-    if (!(IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG)) {
-        printf("Erreur IMG_Init : %s\n", IMG_GetError());
-        SDL_Quit();
-        return 1;
+void dessiner_entite(int type_entite, int position_x, int position_y,int frame) {
+    
+    if (type_entite == 1) {
+        //ajouter des if pour chaque entite pour faire la decoupe de l image pour voir que partie du sprite utiliser
+        SDL_Rect destination = {position_x, position_y, 64, 64};
+        SDL_RenderCopy(renderer, texture_chevre, NULL, &destination);
+    } 
+    else if (type_entite == 2) { 
+        SDL_Rect destination = {position_x, position_y, 64, 64};
+        SDL_RenderCopy(renderer, texture_fermier, NULL, &destination);
     }
-    window = SDL_CreateWindow("Affichage Image", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
-    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    texture_fond = IMG_LoadTexture(renderer, "./img/map.png");
-    texture_chevre = IMG_LoadTexture(renderer, "./img/goat.png");
-    texture_chevreau = IMG_LoadTexture(renderer, "./img/baby_goat.png");
-    texture_fermier = IMG_LoadTexture(renderer, "./img/fermier.png");
-    if (!texture_fond || !texture_chevre || !texture_chevreau || !texture_fermier) {
-        printf("Erreur chargement image : %s\n", IMG_GetError());
-        return 0;
+    else if (type_entite == 3) { 
+        SDL_Rect destination = {position_x, position_y, 64, 64};
+        SDL_RenderCopy(renderer, texture_chevreau, NULL, &destination);
     }
-    return 1;
 }
 
 void dessiner_monde() {
@@ -63,7 +56,7 @@ void dessiner_entite(int type_entite, int x_initial, int y_initial, int x_final,
             SDL_RenderPresent(renderer);
             SDL_Delay(10); 
         }
-        SDL_Rect destination = {x, y, 64, 64};
+        SDL_Rect destination = {prochain_x, prochain_y, 64, 64};
         SDL_RenderCopy(renderer, texture_chevre, NULL, &destination);
     } 
     else if (type_entite == 2) { 
@@ -75,7 +68,7 @@ void dessiner_entite(int type_entite, int x_initial, int y_initial, int x_final,
             SDL_RenderPresent(renderer);
             SDL_Delay(10); 
         }
-        SDL_Rect destination = {x, y, 64, 64};
+        SDL_Rect destination = {prochain_x, prochain_y, 64, 64};
         SDL_RenderCopy(renderer, texture_fermier, NULL, &destination);
     }
     else if (type_entite == 3) { 
@@ -87,7 +80,7 @@ void dessiner_entite(int type_entite, int x_initial, int y_initial, int x_final,
             SDL_RenderPresent(renderer);
             SDL_Delay(10); 
         }
-        SDL_Rect destination = {x, y, 64, 64};
+        SDL_Rect destination = {prochain_x, prochain_y, 64, 64};
         SDL_RenderCopy(renderer, texture_chevreau, NULL, &destination);
     }
 }

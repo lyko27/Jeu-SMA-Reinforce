89e1ff68fafd1aada48aea495e9e0b7028750236
Nateo <bouff3000@gmail.com>
2026-06-19 14:28:34 +0200

    fin de la latence sur le déplacement du fermier

--- utilisateur.c
+++ utilisateur.c
@@ -12,32 +12,29 @@ interaction_utilisateur * recuperer_mouvement(interaction_utilisateur *resultat)
         case SDL_QUIT:
             resultat->quitter = 1;
             break;
-
-        case SDL_KEYDOWN:
-            break;
         default:
             break;
         }
-        if (Keyborad[SDL_SCANCODE_SPACE])
-        {
-            resultat->pause = 1;
-        }
-        if (Keyborad[SDL_SCANCODE_UP] || Keyborad[SDL_SCANCODE_Z] || Keyborad[SDL_SCANCODE_W])
-        {
-            resultat->y_deplacement = 1;
-        }
-        if (Keyborad[SDL_SCANCODE_DOWN] || Keyborad[SDL_SCANCODE_S])
-        {
-            resultat->y_deplacement = -1;
-        }
-        if (Keyborad[SDL_SCANCODE_LEFT] || Keyborad[SDL_SCANCODE_Q] || Keyborad[SDL_SCANCODE_A])
-        {
-            resultat->x_deplacement = 1;
-        }
-        if (Keyborad[SDL_SCANCODE_RIGHT] || Keyborad[SDL_SCANCODE_D])
-        {
-            resultat->x_deplacement = -1;
-        }
+    }
+    if (Keyborad[SDL_SCANCODE_SPACE])
+    {
+        resultat->pause = 1;
+    }
+    if (Keyborad[SDL_SCANCODE_UP] || Keyborad[SDL_SCANCODE_Z] || Keyborad[SDL_SCANCODE_W])
+    {
+        resultat->y_deplacement = 1;
+    }
+    if (Keyborad[SDL_SCANCODE_DOWN] || Keyborad[SDL_SCANCODE_S])
+    {
+        resultat->y_deplacement = -1;
+    }
+    if (Keyborad[SDL_SCANCODE_LEFT] || Keyborad[SDL_SCANCODE_Q] || Keyborad[SDL_SCANCODE_A])
+    {
+        resultat->x_deplacement = 1;
+    }
+    if (Keyborad[SDL_SCANCODE_RIGHT] || Keyborad[SDL_SCANCODE_D])
+    {
+        resultat->x_deplacement = -1;
     }
     return resultat;
 }

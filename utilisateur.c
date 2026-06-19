360437340010cf9a9103bc0db111799390b7b1a5
Nateo <bouff3000@gmail.com>
2026-06-19 14:17:34 +0200

    utilisation de GetkeyboardState pour les déplacements

--- utilisateur.c
+++ utilisateur.c
@@ -3,6 +3,7 @@
 
 interaction_utilisateur * recuperer_mouvement(interaction_utilisateur *resultat)
 {
+    const Uint8 * Keyborad = SDL_GetKeyboardState(NULL);
     SDL_Event event;
     while (SDL_PollEvent(&event))
     {
@@ -13,23 +14,23 @@ interaction_utilisateur * recuperer_mouvement(interaction_utilisateur *resultat)
             break;
 
         case SDL_KEYDOWN:
-            if (event.key.keysym.sym == SDLK_SPACE)
+            if (Keyborad[SDL_SCANCODE_SPACE])
             {
                 resultat->pause = 1;
             }
-            else if (event.key.keysym.sym == SDLK_UP)
+            if (Keyborad[SDL_SCANCODE_UP] || Keyborad[SDL_SCANCODE_Z] || Keyborad[SDL_SCANCODE_W])
             {
                 resultat->y_deplacement = 1;
             }
-            else if (event.key.keysym.sym == SDLK_DOWN)
+            if (Keyborad[SDL_SCANCODE_DOWN] || Keyborad[SDL_SCANCODE_S])
             {
                 resultat->y_deplacement = -1;
             }
-            else if (event.key.keysym.sym == SDLK_LEFT)
+            if (Keyborad[SDL_SCANCODE_LEFT] || Keyborad[SDL_SCANCODE_Q] || Keyborad[SDL_SCANCODE_A])
             {
                 resultat->x_deplacement = 1;
             }
-            else if (event.key.keysym.sym == SDLK_RIGHT)
+            if (Keyborad[SDL_SCANCODE_RIGHT] || Keyborad[SDL_SCANCODE_D])
             {
                 resultat->x_deplacement = -1;
             }

a1a0b4e4d3d3d4dcd0a4ee283bd4dd0d8ead0d33
Nateo <bouff3000@gmail.com>
2026-06-21 16:40:24 +0200

    ajout de la fonction de calcul de perception dans mnde.c

--- goat.c
+++ goat.c
@@ -2,6 +2,8 @@
 #include <time.h>
 #include <math.h>
 #include "goat.h"
+#include "monde.h"
+#include "loup.h"
 
  /**
  * Synopsis : Décide de la prochaine action de la chèvre en fonction de sa perception.
@@ -37,23 +39,57 @@ ActionGoat decider_action(Goat *g, PerceptionGoat p) {
     return g->action_choisi;
 }
 
-void evaluer_interets(Goat *g, PerceptionGoat p) 
+Goat * evaluer_interets(Goat *current_goat, PerceptionGoat perception) 
 {
     // intérêts par défaut
-    g->table_interets[ACTION_ERRER] = 0.0;
-    g->table_interets[ACTION_FUIR_LOUP] = 0.0;
+    current_goat->table_interets[ACTION_ERRER] = 0.0;
+    current_goat->table_interets[ACTION_FUIR_LOUP] = 0.0;
 
     // modif selon la Perception
-    if (p.dist_loup_proche < 150.0) 
+    if (perception.dist_loup_proche < 150.0) 
     {
         // on fuit si le loup est très proche
-        g->table_interets[ACTION_FUIR_LOUP] = 10.0 - (p.dist_loup_proche / 15.0);
-        g->table_interets[ACTION_ERRER] = -10.0f;
+        current_goat->table_interets[ACTION_FUIR_LOUP] = 10.0 - (perception.dist_loup_proche / 15.0);
+        current_goat->table_interets[ACTION_ERRER] = -10.0f;
     }
     else
     {
         // sinon la chèvre chill
-        g->table_interets[ACTION_FUIR_LOUP] = -10.0;
-        g->table_interets[ACTION_ERRER] = 10.0;
+        current_goat->table_interets[ACTION_FUIR_LOUP] = -10.0;
+        current_goat->table_interets[ACTION_ERRER] = 10.0;
     }
+    return current_goat;
+}
+
+/*
+    Entrée : le monde actuel et la chèvre dont on veut calculer la perception
+    Sortie : la perception de la chèvre
+    Synopsis : Calcule la perception d'une chèvre par rapport à son environnement.
+ */
+PerceptionGoat * calculer_perception_goat(monde * monde_courant,Goat * current_goat)
+{
+    PerceptionGoat * perception_goat = malloc(sizeof(PerceptionGoat));
+    int indice_loup_proche = 0;
+    int distance_loup_plus_proche = 1000;
+    for (int i = 0; i<monde_courant->nb_wolf; i++)
+    {
+        Wolf * current_wolf = monde_courant->wolfs_tab[i];
+        int distance_x = current_goat->x-current_wolf->x;
+        int distance_y = current_goat->y-current_wolf->y;
+        int distance = sqrt(distance_x*distance_x + distance_y*distance_y);
+        if (distance < distance_loup_plus_proche)
+        {
+            distance_loup_plus_proche = distance;
+            indice_loup_proche = i;
+        }
+    }
+    perception_goat->dist_loup_proche = distance_loup_plus_proche;
+    perception_goat->pos_x_loup = monde_courant->wolfs_tab[indice_loup_proche]->x;
+    perception_goat->pos_y_loup = monde_courant->wolfs_tab[indice_loup_proche]->y;
+    perception_goat->goats_tab = monde_courant->goats_tab;
+    perception_goat->capacite_max_goat = monde_courant->capacite_max_goat;
+    perception_goat->nb_goat = monde_courant->nb_goat;
+    perception_goat->pos_x_fermier = monde_courant->fermiers->x;
+    perception_goat->pos_y_fermier = monde_courant->fermiers->y;
+    return perception_goat;
 }
